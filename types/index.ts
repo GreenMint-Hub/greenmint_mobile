6c97d4e48829141dc527d6b43bf5aa52d0a18a7d
asimwe1 <landryasimwe@gmail.com>
2025-07-05 14:27:00 +0200

    Add utility functions in utils/helpers.ts

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@
 // Updated on 2025-07-07
 // Updated on 2025-07-07
 // Updated on 2025-07-07
+// Updated on 2025-07-07
 export type EcoActivity = {
   id: string;
   type: 'cycling' | 'publicTransport' | 'recycling' | 'energySaving' | 'plantBasedMeal' | 'secondHandPurchase';
